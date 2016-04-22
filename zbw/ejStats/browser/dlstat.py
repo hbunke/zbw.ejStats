8e1ee94fea0c90e43e5aae2f3db3a13c3ee3239e
Hendrik Bunke <h.bunke@zbw.eu>
2016-04-22 20:23:13 +0200

    more succinct and functional dlstat

--- zbw/ejStats/browser/dlstat.py
+++ zbw/ejStats/browser/dlstat.py
@@ -5,21 +5,14 @@ from zope.annotation.interfaces import IAnnotations
 from plone.memoize.view import memoize
 from Products.ATContentTypes.utils import DT2dt
 import datetime
-from operator import div, lt, eq, itemgetter
-
-
-# FOR LATER USE
-#from reportlab.graphics.shapes import Drawing
-#from reportlab.graphics.charts.linecharts import HorizontalLineChart
-#from reportlab.graphics.widgets.markers import uSymbol2Symbol, isSymbol, makeMarker
-
+from operator import sub, gt
 from Products.CMFCore.utils import getToolByName
 from iqpp.clickcounting.interfaces import IClickCounting
 from zbw.ejStats.utils import format_number
 
 # we could use chain.from_iterable here, but toolz.concat is shorter (and uses
 # chain.from_iterables)
-#from itertools import chain
+# from itertools import chain
 from toolz.itertoolz import concat
 from toolz.functoolz import pipe
 from toolz.dicttoolz import keymap
@@ -31,7 +24,7 @@ def clickcount(obj):
 
 
 def average(papers, downloads):
-  #  avg = lambda dl: {gt(dl, 0): div(dl, papers), eq(dl, 0): 0}.get(True)
+    # avg = lambda dl: {gt(dl, 0): div(dl, papers), eq(dl, 0): 0}.get(True)
     avg = lambda dl: {dl > 0: dl / papers, dl == 0: 0}.get(True)
     return avg(downloads)
 
@@ -58,12 +51,10 @@ class DownloadStatistic(BrowserView):
         self.request.set('disable_border', True)
         self.statsview = getMultiAdapter((self.context, self.request),
                 name="stats")
-
-        if not self.request.has_key('days') or self.request['days'] == "":
+        if 'days' not in self.request or self.request['days'] == "":
             self.request.set('days', 30)
 
         return self.template()
-    
     
     def get_brains(self, pt, **kwargs):
         catalog = getToolByName(self.context, 'portal_catalog')
@@ -82,59 +73,43 @@ class DownloadStatistic(BrowserView):
         
         return objects
 
-    
     def __was_new(self, dates, created):
         """
         checks if article was recent when download occured
         """
-        #days an article is considered to be new
-        #delta = 30 
         delta = int(self.request['days'])
-
-        #convert old Zope DateTime to python datetime
+        # convert old Zope DateTime to python datetime
         obj_date = DT2dt(created).replace(tzinfo=None)
-        
         d = datetime.timedelta(days=delta)
-        
-        counter = 0
-        for clickdate in dates:
-            e = clickdate - d
-            if obj_date > e:
-                counter += 1
-        
-        return counter
+        return len(filter(lambda date: gt(obj_date, sub(date, d)), dates))
 
     @memoize
     def __dl_over_time(self):
         """
         get a sorted dictionary with month as key
         """
-        annotations = lambda obj: IAnnotations(obj)['hbxt.clickdates']
-        dates = map(annotations, self.get_clickdates_objects())
-        keyse = concat(map(lambda date: map(lambda k: k, date.keys()), dates))
-        dl_gesamt = {k: {'Sum': 0, 'new': 0, 'JournalPaper': 0,
-            'DiscussionPaper': 0} for k in keyse}
-
         # XXX can we do this more functional?
         def update_dic(obj):
-            d = dl_gesamt
             dates = annotations(obj)
             for key in dates.keys():
                 count = len(dates[key])
-                d[key]['Sum'] += count
-                d[key][obj.portal_type] += count
-                was_new = self.__was_new(dates[key], obj.created())
-                d[key]['new'] += was_new
-        
+                dl[key]['Sum'] += count
+                dl[key][obj.portal_type] += count
+                dl[key]['new'] += self.__was_new(dates[key], obj.created())
+
+        annotations = lambda obj: IAnnotations(obj)['hbxt.clickdates']
+        dates = map(annotations, self.get_clickdates_objects())
+        keyse = concat(map(lambda date: map(lambda k: k, date.keys()), dates))
+        dl = {k: {'Sum': 0, 'new': 0, 'JournalPaper': 0,
+            'DiscussionPaper': 0} for k in keyse}
         map(update_dic, self.get_clickdates_objects())
-        return keymap(add_leading_zero, dl_gesamt)
+        return keymap(add_leading_zero, dl)
         
-                
     def dl_listed(self):
         """
         """
         dl_sorted = self.__dl_over_time()
-        months = sorted(dl_sorted.iterkeys(),reverse=True)
+        months = sorted(dl_sorted.iterkeys(), reverse=True)
         return map(lambda m: (m, dl_sorted[m]), months)
 
     @memoize
@@ -186,40 +161,3 @@ class DownloadStatistic(BrowserView):
         return average(papers, downloads)
 
 
-    #def dl_minmax(self):
-        #     """
-        #     """
-        #     dl_sorted = self.__dl_over_time()
-        #     dlvalues = dl_sorted.values()
-        #     dlvalues = sorted(dlvalues)
-        #     dl_min = dlvalues[0]
-        #     dl_max = dlvalues[-1]
-        #     #import pdb; pdb.set_trace()
-        #     return (dl_min, dl_max)
-
-
-
-        # def dl_chart(self):
-        #     """
-        #     """
-        #     drawing = Drawing(200, 100)
-
-        #     data = [ 
-        #             (13, 5, 20, 22, 37, 45, 19, 4), 
-        #             (14, 10, 21, 28, 38, 46, 25, 5)
-        #             ]   
-
-        #     lc = HorizontalLineChart()
-
-        #     lc.x = 20
-        #     lc.y = 10
-        #     lc.height = 85
-        #     lc.width = 170 
-        #     lc.data = data
-        #     lc.lines.symbol = makeMarker('Circle')
-
-        #     drawing.add(lc)
-        #         
-        #     drawing.save(fnRoot='test', formats=['png'])
-        #     return drawing
-        
